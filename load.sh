3411bd63bd323e45d90a10836363b956fc5db28e
Alex Spitzer <spitzer@cs.washington.edu>
2022-11-16 21:47:16 -0800

    Update load.sh

--- load.sh
+++ load.sh
@@ -2,10 +2,12 @@
 
 set -e
 
-if [ ! -f ${HOME}/.vimrc ]; then
-  ln -s vimrc ${HOME}/.vimrc
+if [ ! -f "${HOME}/.vimrc" ]; then
+  ln -s vimrc "${HOME}/.vimrc"
 else
   echo "${HOME}/.vimrc already exists. not overwriting. symlink manually".
 fi
 
-mkdir -p ${HOME}/.vim/undo
+mkdir -p "${HOME}/.vim/undo"
+
+cp mvlatest "${HOME}/.local/bin"
