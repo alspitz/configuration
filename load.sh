f0b506a2a08f369aa50ebc337f72c815010ec66a
Alex Spitzer <aes368@cornell.edu>
2024-03-09 14:41:58 -0800

    Add bashrc and dircolors

--- load.sh
+++ load.sh
@@ -2,11 +2,18 @@
 
 set -e
 
+echo "Loading vimrc, bashrc, dircolor, setting up vim undo and mvlatest"
+
 if [ ! -f "${HOME}/.vimrc" ]; then
   ln -s "$(pwd)/vimrc" "${HOME}/.vimrc"
 else
   echo "${HOME}/.vimrc already exists. not overwriting. symlink manually".
 fi
+
+cp "${HOME}/.bashrc" "${HOME}/.bashrc.backup"
+ln -sf "$(pwd)/bashrc" "${HOME}/.bashrc"
+
+ln -sf "$(pwd)/dircolors" "${HOME}/.dircolors"
 
 mkdir -p "${HOME}/.vim/undo"
 
